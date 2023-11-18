f781a5da64395d1e9d9e995bc509326b0d4cbe14
WilzD <wilsondynamic3@gmail.com>
2023-11-18 22:46:06 +0530

    sending a msg and showing on top of input box using file reading - writing

---
+++ writing-reading-file.js
@@ -0,0 +1,34 @@
+const http=require('http')
+const fs=require('fs')
+
+const server=http.createServer((req,res)=>{
+const url=req.url
+const method=req.method
+if(url==='/'){
+      fs.readFile('message.txt','utf8',function(err,data){
+      res.write('<html>')
+      res.write('<head><title>Write a Good message</title></head>')
+      res.write(`<body><div>${data}</div><form action="/message" method="POST">Write a Message<input type="text" name="message"><button type="submit">Send</button></form></body>`)
+      res.write('</html>')
+      return res.end()
+    });
+}
+if(url==='/message' && method==='POST'){
+    const body=[]
+    req.on('data',(chunk)=>{
+      console.log(chunk)
+     body.push(chunk)
+    })
+    return req.on('end',()=>{
+      const parsedBody=Buffer.concat(body).toString()
+      const message=parsedBody.split('=')[1]
+      fs.writeFile('message.txt',message, err=>{
+        res.statusCode=302
+        res.setHeader('Location','/')
+        return res.end()
+      })
+    })
+  }
+})
+
+server.listen(4000)
